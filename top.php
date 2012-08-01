ad8b376c2a10d494490b5acf52841265e481c9a8
Nick Jones <nick@sigfrid.co.uk>
2012-07-31 18:00:22 -0700

    All SWOM specific options and references now in seperate class

--- top.php
+++ top.php
@@ -1,7 +1,7 @@
 <div id="category-head">
 <?php 
-$sigf_frame->sigf_generate_featured();
-$sigf_frame->sigf_generate_headlines();
+$swom_theme->sigf_generate_featured('featured');
+$swom_theme->sigf_generate_headlines('cat_pages','cat_order', 'max_cat','empty_cat','head_img' ,array('home_no'=>is_home(),'post_no'=>!is_home()));
 ?>
 	<div class="clear"></div>
 </div>
