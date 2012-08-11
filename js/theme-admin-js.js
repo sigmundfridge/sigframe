4d0f2fd2bbbf1070b770001a68a27a5b5a3250a2
Nick Jones <nick@sigfrid.co.uk>
2012-08-10 21:12:26 -0700

    Finished version of sigf framework
    Removed all theme files not related to framework. Theme will be kept (along with framework backend) in a different repository.

---
+++ js/theme-admin-js.js
@@ -0,0 +1,54 @@
+(function($) {
+    $('html').addClass('js');
+
+	$(document).ready(function() {
+
+		var result = $('#cat_order').sortable({
+			axis:'y',
+			containment:'document',
+		})
+		.parents('.cat_menu').addClass('cat_input')
+
+		$("#sigf_loader").fadeOut().remove();
+		$("#tab_wrap").tabs().fadeIn();
+		
+		$('.saved').animate({opacity: 0}, 3000, function(){$(this).css('display','none')});
+		
+		window.formfield='';
+		$('.upload_image_button').click(function() {
+			window.formfield = $('.upload_field', $(this).parent());
+			tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
+			return false;
+		});
+	
+		window.send_to_editor = function(html) {
+			imgurl = $('img',html).attr('src');
+			window.formfield.val(imgurl);
+			tb_remove();
+			preview_img(window.formfield);
+		};
+	
+		$('.upload_field').change(function(){
+			preview_img($(this));
+		})
+	
+	
+		function preview_img(selector){
+			url = $(selector).val()	
+			img_div = jQuery(selector).parent().find('.img-preview');	
+			img_div.animate({opacity: 0}, 100, function() {}).empty();
+			img_hold = img_div.append('<img src="" class = "preview"/>');
+			image = img_hold.find('.preview').attr('src',url);
+			img_div.animate({opacity: 1}, 300, function() {image.animate({opacity: 1}, 300, function() {})});
+		}
+		
+		$deselect = $('.deselect');
+		$deselect.click(function(){
+			if(this.checked){$(this).siblings(':checkbox').removeAttr('checked');}
+			else {}
+		}).siblings().click(function(){
+			if($deselect.checked){}
+			else {$deselect.removeAttr('checked');}
+		})
+	});
+})(jQuery);
