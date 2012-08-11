1a61ebb46a7ffa5c2594d6765022f60959915a27
Nick Jones <nick@sigfrid.co.uk>
2012-08-10 21:16:22 -0700

    Framework seperated from theme files.
    Themes will be housed (with compatible framework files) in different repositories.

--- top.php
+++
@@ -1,7 +0,0 @@
-<div id="category-head">
-<?php 
-$swomTheme->sigf_generate_featured('featured');
-$swomTheme->sigf_generate_headlines('cat_pages','cat_order', 'max_cat','empty_cat','head_img' ,array('home_no'=>is_home(),'post_no'=>!is_home()));
-?>
-	<div class="clear"></div>
-</div>
